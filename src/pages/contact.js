51a135d80793cd78002cfef835b5a6a73e02d07e
David Brodbeck <dav_bro@uni-bremen.de>
2020-02-12 16:43:36 +0100

    added Contact Formula Layout

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Trans, useTranslation, withTranslation} from "react-i18next";
 import Header from "../components/header";
 import Title from "../components/title";
-import {Row, Col, Collapse, Card, Button} from "antd";
+import {Row, Col, Collapse, Card, Button, Form, Input, Icon} from "antd";
 
 import roomPlan from '../static/images/room.jpg';
 import campusPlan from '../static/images/campus.jpg';
+import {Link} from "gatsby";
 
 const { Panel } = Collapse;
+const { TextArea } = Input;
 const floorPlanLink = "https://oracle-web.zfn.uni-bremen.de/web/p_ebenen_ansicht?haus=IW&raum=1310&pi_anz=0";
 const campusPlanLink = "https://www.uni-bremen.de/universitaet/campus/lageplan/";
 
@@ -18,6 +20,8 @@ class ContactClass extends React.Component {
 
     }
 
+
+
     render() {
         const { t } = this.props;
         return(
@@ -27,7 +31,7 @@ class ContactClass extends React.Component {
                 <Row style={{marginTop: "30px"}}>
                     <Col span={14} offset={5}>
 
-                        <Collapse>
+                        <Collapse defaultActiveKey={['1']} >
                             <Panel header={t('contact.office.title')} extra={"IW 1+2, " + t('general.room') + " 1310"} key={1}>
                                 <Trans i18nKey="contact.office.detail">
                                     you can find us in the <strong>IW 1+2</strong>.
@@ -55,8 +59,20 @@ class ContactClass extends React.Component {
                                 </Row>
                             </Panel>
                             <Panel header={t('contact.consultings.title')} key={2}>
-                                {/*every week on <strong>thursday</strong> */}
+                                <Trans i18nKey="contact.consultings.text">
+                                    Jede zweite, ungerade Woche <strong>Donnerstags</strong> bieten wir von <strong> 13:00-15:00 Uhr </strong>
+                                    eine Sprechstunde an. In dieser Zeit könnt ihr uns Fragen rund ums Studium stellen.
+                                    Die genauen Termine findet ihr natürlich auch bei <Link to="dates">Termine</Link>
+                                </Trans>
                             </Panel>
+                            <Panel header={t('contact.formula.title')} key={3}>
+                                <Row>
+                                <Col span={12}>
+                                <WrappedContactForm/>
+                                </Col>
+                                </Row>
+                            </Panel>
+
                         </Collapse>
 
                     </Col>
@@ -71,7 +87,97 @@ class ContactClass extends React.Component {
             loading: false
         });
     }
+
+
 }
+
+function hasErrors(fieldsError) {
+    return Object.keys(fieldsError).some(field => fieldsError[field]);
+}
+
+class ContactFormClass extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    componentDidMount() {
+        this.props.form.validateFields();
+    }
+
+
+    render() {
+
+        const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
+
+        const mailError = isFieldTouched('mail') && getFieldError('mail');
+        const topicError = isFieldTouched('topic') && getFieldError('topic');
+        const messageError = isFieldTouched('message') && getFieldError('message');
+
+        return (
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Item>
+                    <Input
+                        prefix={<Icon type="user" className="form-icon" />}
+                        placeholder="Name"
+                    />
+                </Form.Item>
+                <Form.Item required={true} validateStatus={mailError ? 'error' : ''} help={mailError || ''}>
+                    {getFieldDecorator('mail', {
+                        rules: [{ required: true, message: 'Bitte gib eine gültige Mail Adresse ein!' }],
+                    })(
+                    <Input
+                        prefix={<Icon type="mail" className="form-icon"/>}
+                        placeholder="E-Mail"
+                    />
+                    )}
+                </Form.Item>
+                <Form.Item validateStatus={topicError ? 'warning' : ''} help={topicError || ''}>
+                    {getFieldDecorator('topic', {
+                        rules: [{ required: true, message: 'Bitte gib einen Betreff ein!' }],
+                    })(
+                    <Input
+                        prefix={<Icon type="question" className="form-icon"/>}
+                        placeholder="Betreff"
+                    />)}
+                </Form.Item>
+                <Form.Item validateStatus={messageError ? 'error' : ''} help={messageError || ''}>
+                    {getFieldDecorator('message', {
+                        rules: [{ required: true, message: 'Bitte gib eine Nachricht ein!' }],
+                    })(
+                    <TextArea
+                        placeholder="Deine Nachricht"
+                        rows={4}
+                    />)}
+                </Form.Item>
+                <Form.Item>
+                    <Button icon="login" type="primary" htmlType="submit" disabled={hasErrors(getFieldsError())}>
+                        Absenden
+                    </Button>
+                </Form.Item>
+            </Form>
+        )
+    }
+
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    }
+
+}
+
+
+
+const WrappedContactForm = Form.create({ name: 'contact' })(ContactFormClass);
+
+const ContactForm = withTranslation()(WrappedContactForm);
+
 
 function LinkButton(props) {
     const { t } = useTranslation();
