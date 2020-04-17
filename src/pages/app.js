5ff4bbc2e43e7a2fe71587ae9956fe01fa8db583
David Brodbeck <dav_bro@uni-bremen.de>
2020-04-17 11:51:22 +0200

    fixed Layout deployment error

--- src/pages/app.js
+++ src/pages/app.js
@@ -5,11 +5,10 @@ import Profile from "../components/profile"
 import Login from "../components/login"
 import PrivateRoute from "../components/privateRoute";
 const App = () => (
-    <Layout>
+
         <Router>
             <PrivateRoute path="/app/profile" component={Profile} />
             <Login path="/app/login" />
         </Router>
-    </Layout>
 )
 export default App
